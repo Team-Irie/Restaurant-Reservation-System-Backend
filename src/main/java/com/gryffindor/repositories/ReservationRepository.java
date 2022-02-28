3e7444b702fe9078d74607d75aa45020afe46cd7
DominicSieli <dominicsieli@gmail.com>
2022-02-28 14:45:13 -0700

    got most crud methods working

--- src/main/java/com/gryffindor/repositories/ReservationRepository.java
+++ src/main/java/com/gryffindor/repositories/ReservationRepository.java
@@ -1,15 +1,12 @@
 package com.gryffindor.repositories;
 
 import java.util.List;
-
 import com.gryffindor.models.User;
 import com.gryffindor.models.Reservation;
-import com.gryffindor.types.ReservationStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
     List<Reservation> getReservationsByCustomer(User user);
-    List<Reservation> getReservationsByStatus(ReservationStatus reservationStatus);
 }
