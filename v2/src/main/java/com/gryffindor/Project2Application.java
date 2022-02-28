9bb21a9c86f5b9cce53d50916bb7ca78a87b12fc
Arby Gudes <aegudes@gmail.com>
2022-02-28 10:54:52 -0600

    adds list of tasks to complete for v2

--- v2/src/main/java/com/gryffindor/Project2Application.java
+++ v2/src/main/java/com/gryffindor/Project2Application.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Project2Application {
-
+	/*
+	* import mockito
+	* import hibernate
+	* import spring data
+	* import log4j
+	* make a new db for project
+	* make config file for hibernate
+	* */
 	public static void main(String[] args) {
 		SpringApplication.run(Project2Application.class, args);
 	}
-
 }
