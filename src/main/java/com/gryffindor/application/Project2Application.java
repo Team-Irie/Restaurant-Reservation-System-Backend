a49567670788b72d3359337558e44eff9168bd2e
KHADGA-R <parajuli.kr@gmail.com>
2022-02-28 17:41:17 -0500

    MailService

--- src/main/java/com/gryffindor/application/Project2Application.java
+++ src/main/java/com/gryffindor/application/Project2Application.java
@@ -1,15 +1,33 @@
 package com.gryffindor.application;
 
+import com.gryffindor.services.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 //@ComponentScan("com.gryffindor")
 public class  Project2Application {
+
+
+	@Autowired
+	private EmailSenderService senderService;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(Project2Application.class, args);
 	}
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMail(){
+		senderService.sendEmail("parajuli.kr@gmail.com",
+				"Reservation Confirmation",
+				"Your reservation request has been confirmed");
+
+	}
+
 }
 
 /*
