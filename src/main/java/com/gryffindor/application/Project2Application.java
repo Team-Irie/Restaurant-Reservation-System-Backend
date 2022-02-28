68f29f9aff6a387a582b1b947dc8652a80421bdc
DominicSieli <dominicsieli@gmail.com>
2022-02-28 09:46:07 -0700

    Created LoggingAspect

--- src/main/java/com/gryffindor/application/Project2Application.java
+++ src/main/java/com/gryffindor/application/Project2Application.java
@@ -2,41 +2,10 @@ package com.gryffindor.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan("com.gryffindor")
 public class  Project2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Project2Application.class, args);
 	}
 }
-
-/*
-*
-* application is not able to recognize components because of directory structure
-* components must be in either:
-* 1. same directory as main application
-* 2. subdirectory within the main application directory
-*
-* got this error when using @ComponentScan, or when trying to rearrange directory structure:
-*
-*
-* ***************************
-APPLICATION FAILED TO START
-***************************
-
-Description:
-
-Parameter 0 of constructor in com.gryffindor.controllers.ReservationController required a bean of type 'com.gryffindor.services.ReservationService' that could not be found.
-
-
-Action:
-
-Consider defining a bean of type 'com.gryffindor.services.ReservationService' in your configuration.
-*
-*
-* will not compile when controllers are referenced in @ComponentScan
-*
-* */
-
