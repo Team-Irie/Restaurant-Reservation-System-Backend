dc27b22f4ce0280389ef57ac2a53ff9daa5d5e88
Arby Gudes <aegudes@gmail.com>
2022-02-28 15:45:30 -0600

    pausing point stage 3
    db is connected strange behehavoir with tables noted

--- v2/src/main/java/com/gryffindor/repository/ReservationRepository.java
+++ v2/src/main/java/com/gryffindor/repository/ReservationRepository.java
@@ -1,4 +1,9 @@
 package com.gryffindor.repository;
 
-public class ReservationRepository {
+import com.gryffindor.models.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
 }
--- v2/src/main/java/com/gryffindor/services/ReservationService.java
+++ v2/src/main/java/com/gryffindor/services/ReservationService.java
@@ -1,8 +1,15 @@
 package com.gryffindor.services;
 
+import com.gryffindor.models.Reservation;
 import com.gryffindor.repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.util.List;
 
+
+@Service
+@Transactional
 public class ReservationService {
 
     private ReservationRepository rr;
@@ -13,5 +20,19 @@ public class ReservationService {
     @Autowired
     public ReservationService(ReservationRepository rr) {
         this.rr = rr;
+    }
+
+    //public create()
+
+    public List<Reservation> getAll(){
+        return rr.findAll();
+    }
+
+    public void update(Reservation reservation){
+        rr.save(reservation);
+    }
+
+    public void delete(Reservation reservation){
+        rr.delete(reservation);
     }
 }
