501bbb974827275493014eaf54199424fbcf1eed
DominicSieli <dominicsieli@gmail.com>
2022-03-01 07:52:14 -0700

    Fixed LoggingAspect, added reservation status updated methods

--- src/main/java/com/gryffindor/services/ReservationService.java
+++ src/main/java/com/gryffindor/services/ReservationService.java
@@ -3,6 +3,7 @@ package com.gryffindor.services;
 import java.util.List;
 import com.gryffindor.models.User;
 import com.gryffindor.models.Reservation;
+import com.gryffindor.types.ReservationStatus;
 import com.gryffindor.repositories.ReservationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,26 @@ public class ReservationService {
 
     public Reservation getReservationById(int id) {
         return reservationRepository.getById(id);
+    }
+
+    public void approveReservation(Reservation reservation) {
+        reservation.setReservationStatus(ReservationStatus.APPROVED);
+        reservationRepository.save(reservation);
+    }
+
+    public void denyReservation(Reservation reservation) {
+        reservation.setReservationStatus(ReservationStatus.DENIED);
+        reservationRepository.save(reservation);
+    }
+
+    public void cancelReservation(Reservation reservation) {
+        reservation.setReservationStatus(ReservationStatus.CANCELED);
+        reservationRepository.save(reservation);
+    }
+
+    public void fulfillReservation(Reservation reservation) {
+        reservation.setReservationStatus(ReservationStatus.FULFILLED);
+        reservationRepository.save(reservation);
     }
 
     public void updateReservation(Reservation reservation) {
