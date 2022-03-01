58d9b4b5efb196e0af62d9129df5730180ec4a2f
Ryan Sy <ryanbsy@gmail.com>
2022-03-01 18:02:39 -0500

    refactored - created SearchYelpService

--- src/main/java/com/gryffindor/controllers/SearchYelpController.java
+++ src/main/java/com/gryffindor/controllers/SearchYelpController.java
@@ -1,34 +1,15 @@
 package com.gryffindor.controllers;
 
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+import com.gryffindor.services.SearchYelpService;
 
 @RestController
 @CrossOrigin("*")
 public class SearchYelpController {
 
+
     @GetMapping("/search")
     public String searchYelp(@RequestParam String term, @RequestParam String location) {
-        // send GET request to yelp api and return results here
-        // request - query parameters(term, location) and url
-        String url = "https://api.yelp.com/v3/businesses/search?term=" + term + "&location=" + location + "&limit=1";
-
-        // create headers
-        HttpHeaders headers = new HttpHeaders();
-        String yelpApiKey = System.getenv("YELP_API_KEY");
-        headers.setBearerAuth(yelpApiKey);
-
-        // create request
-        HttpEntity<?> request = new HttpEntity<>(headers);
-
-        // make a request
-        ResponseEntity<String> response = new RestTemplate().exchange(url, HttpMethod.GET, request, String.class);
-
-        // get JSON response
-        String json = response.getBody();
-        System.out.print(url);
-
-        return json;
+        return SearchYelpService.searchYelp(term, location);
     }
 }
