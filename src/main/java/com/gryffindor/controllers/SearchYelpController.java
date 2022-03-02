fafe3655bfdb73d59567b3134f0f463cb43eaae9
Ryan Sy <ryanbsy@gmail.com>
2022-03-02 15:35:17 -0500

    modified build.gradle/hibernate.cfg.xml to use environment variables for db connecton

--- src/main/java/com/gryffindor/controllers/SearchYelpController.java
+++ src/main/java/com/gryffindor/controllers/SearchYelpController.java
@@ -6,7 +6,7 @@ import com.gryffindor.services.SearchYelpService;
 @RestController
 @CrossOrigin("*")
 public class SearchYelpController {
-    
+
     @GetMapping("/search")
     public String searchYelp(@RequestParam String term, @RequestParam String location) {
         return SearchYelpService.searchYelp(term, location);
